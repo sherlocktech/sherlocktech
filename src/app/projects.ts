6aa09825085cbd850743b559f2beb816bc28876d
Sean Stamm <sean.stamm5@gmail.com>
2018-07-20 15:26:50 -0700

    Add descriptions for projects

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -9,14 +9,17 @@ export interface Project {
 export const PROJECTS: Project[] = [
     {
         name: 'Purpose Project',
-        description: 'Project to help users',
+        description: `Angular application that enables users to live a purpose driven life
+         by offering a platform to explore their goals and values. Encompasses a blog
+         that offers advice and insight into other's prupose projects.`,
         url: 'https://purpose-project.firebaseapp.com',
         githubURL: 'https://github.com/christinamcmahon/purpose-project',
         image: '../assets/img/purpose_project.PNG'
     },
     {
         name: 'Trip Calculator',
-        description: 'Project to help users',
+        description: `Angular application that helps those with travelling budgets to plan
+         trips that are within their means.`,
         url: 'https://trip-calculator-5.firebaseapp.com',
         githubURL: 'https://github.com/sherlocktech/trip-calculator',
         image: '../assets/img/trip_calculator.PNG'
